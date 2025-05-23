935d5149cb18173ff0e99f6ec4ab106dcf6cb965
Adam Tyler <lazy9669@gmail.com>
2025-05-23 11:00:11 -0500

    launch args

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { LaunchArguments } from "react-native-launch-arguments";
+
+const args = LaunchArguments.value();
+
+console.log("Launch Arguments: ", args);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Launch Arguments:</Text>
+      <Text>{JSON.stringify(args)}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,17 @@
+# To test this in a simulator/emulator:
+
+Make sure your env is good to run Expo apps locally. This is using Expo development builds in order to utilize the `react-native-launch-arguments` library.
+
+## iOS
+
+1. Prebuild native directories `npm run clean`
+2. Build/Run iOS app: `npm run ios`
+3. Close app on simulator once opened and running
+4. Launch app: `xcrun simctl launch booted com.adam.tyler.launchargstest -param "HELLO" -param2 "Adam"`
+
+## Android
+
+1. Prebuild native directories `npm run clean`
+2. Build/Run Android app: `npm run android`
+3. Close app on emulator once opened and running
+4. Launch app: `adb shell am start -W -n com.adam.tyler.launchargstest/.MainActivity -S --es param HELLO --es param2 Adam`
